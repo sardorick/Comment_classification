2330db645785f6ccb1888fedc49f81988b7ce810
sardorbekzokirov <45316437+sardorick@users.noreply.github.com>
2022-06-23 12:26:43 +0200

    Update model.py

--- model.py
+++ model.py
@@ -19,24 +19,25 @@ class MultiClassifier(nn.Module):
         self.out6 = nn.Linear(256, 1)
 
     def forward(self,x):
+        x = x.view(x.shape[0], -1 )
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
 
         # output nodes for each different label
-        out1 = F.sigmoid(self.out1(x))
-        out2 = F.sigmoid(self.out2(x))
-        out3 = F.sigmoid(self.out3(x))
-        out4 = F.sigmoid(self.out4(x))
-        out5 = F.sigmoid(self.out5(x))
-        out6 = F.sigmoid(self.out6(x))
+        out1 = torch.sigmoid(self.out1(x))
+        out2 = torch.sigmoid(self.out2(x))
+        out3 = torch.sigmoid(self.out3(x))
+        out4 = torch.sigmoid(self.out4(x))
+        out5 = torch.sigmoid(self.out5(x))
+        out6 = torch.sigmoid(self.out6(x))
 
         return out1, out2, out3, out4, out5, out6
     
-model = MultiClassifier(32, 300)
+# model = MultiClassifier(32, 300)
 
-data = torch.rand(16, 32, 300)
+# data = torch.rand(16, 32, 300)
 
-output = model.forward(data)
-print(output)
+# output = model.forward(data)
+# print(len(output))
